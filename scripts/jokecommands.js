0078cddf35405cea33bea314fcba1690be9496e1
wfe-the-anima <garyoakhart@gmail.com>
2015-11-25 20:04:39 +0000

    Update jokecommands.js

--- scripts/jokecommands.js
+++ scripts/jokecommands.js
@@ -3,3 +3,7 @@ exports.handleCommand = function(src, command, commandData, tar, channel) {
         normalbot.sendAll("" + sys.name(src) + "is a fake developer now!");
         return;
 }
+exports.help = 
+    [
+        "/dev: Become a fake developer!"
+    ];
