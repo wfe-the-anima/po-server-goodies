b727669cfc0698885e72ccd7dd1f1384b704e931
wfe-the-anima <garyoakhart@gmail.com>
2015-11-26 21:50:47 +0000

    Update jokecommands.js

--- scripts/jokecommands.js
+++ scripts/jokecommands.js
@@ -5,7 +5,7 @@ exports.handleCommand = function (src, command, commandData, tar, channel) {
     }
     if (command == "slap") {
         if (commandData === undefined) {
-            normalbot.sendAll("You can't slap somebody who don't exist!");
+            normalbot.sendAll(src, "You can't slap somebody who don't exist!", channel);
         } else {
             normalbot.sendAll(sys.name(src) + " slapped " + commandData + " for being somewhat of a b****!", channel);
             return;
