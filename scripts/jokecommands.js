f6c978b6c19a75a3aab0d0010faf742d75cbb726
wfe-the-anima <garyoakhart@gmail.com>
2015-11-26 17:55:48 +0000

    Update jokecommands.js

--- scripts/jokecommands.js
+++ scripts/jokecommands.js
@@ -1,14 +1,15 @@
-exports.handleCommand = function(src, command, commandData, tar, channel) {
+exports.handleCommand = function (src, command, commandData, tar, channel) {
     if (command == "dev") {
-        normalbot.sendAll("" + sys.name(src) + " is a fake developer now!", channel);
+        normalbot.sendAll(sys.name(src) + " is a fake developer now!", channel);
         return;
     }
     if (command == "slap") {
-        if (commandData == undefined) {
-            normalbot.sendAll("You can't slap somebody which don't exist!");
+        if (commandData === undefined) {
+            normalbot.sendAll("You can't slap somebody who don't exist!");
         } else {
-        normalbot.sendAll("" + sys.name(src) + " slapped " + commandData + " for being somewhat of a b****!", channel);
-        return;
+            normalbot.sendAll(sys.name(src) + " slapped " + commandData + " for being somewhat of a b****!", channel);
+            return;
+	    }
     }
     return "no command";
 };
